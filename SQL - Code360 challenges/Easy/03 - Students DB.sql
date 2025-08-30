aab8cfc46cb83fd5b27ce4812b7bb9c4d7d08df7
BenGyde <Bengyde@gmail.com>
2025-08-30 12:33:03 +0200

    Create 03 - Students DB.sql

---
+++ SQL - Code360 challenges/Easy/03 - Students DB.sql
@@ -0,0 +1,18 @@
+-- Insert below student details in students table and print all data of table.
+
+--+---------+--------+-------+
+--| ID  |  Name       | Gender|
+--+---------+--------+-------+
+--|   3     |  Kim    |   F   |
+--|   4     | Molina  |   F   |
+--|   5     | Dev     |   M   |
+--+---------+--------+-------+
+
+INSERT INTO students (id, name, gender)
+VALUES 
+    (3, 'Kim', 'F'),
+    (4, 'Molina', 'F'),
+    (5, 'Dev', 'M');
+
+SELECT *
+FROM students
